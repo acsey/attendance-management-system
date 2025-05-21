910b667de1480e71ff17b38e2018e5db1f75fbeb
David <sanchez.david@me.com>
2025-05-21 11:23:11 -0600

    Add .next/static/chunks/src_app_page_tsx_e3c30bf1._.js 1747848191233

---
+++ .next/static/chunks/src_app_page_tsx_e3c30bf1._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1ff4e8f1._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_html5-qrcode_esm_62b22b9a._.js",
+  "static/chunks/node_modules_html5-qrcode_third_party_zxing-js_umd_df022588.js",
+  "static/chunks/node_modules_05abaefb._.js"
+],
+    source: "dynamic"
+});
